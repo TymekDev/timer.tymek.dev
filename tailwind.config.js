28e2b2275785fb1da7b2bde44a569b54bb2b543f
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-06-07 00:17:52 +0200

    refactor: move source code into src/

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import daisyui from "daisyui";
 
 /** @type {import("tailwindcss").Config} */
 export default {
-  content: ["./index.html"],
+  content: ["src/index.html"],
   theme: {},
   plugins: [daisyui],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 /** @type {import("vite").UserConfig} */
 export default {
+  root: "src/",
   build: {
     target: "ES6",
+    outDir: "../dist",
+    emptyOutDir: true
   },
 };
