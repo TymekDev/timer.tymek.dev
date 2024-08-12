8a46c82cb2e2febd5fc274135494eb2aab46dd63
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-08-12 09:57:03 +0200

    refactor: use Countdown.set

--- src/index.js
+++ src/index.js
@@ -4,26 +4,16 @@ const countdown = /** @type {Countdown} */ (
   document.getElementById("countdown")
 );
 
-/**
- * @param {Object} hms
- * @param {number} hms.seconds
- * @param {number} hms.minutes
- * @param {number} hms.hours
- */
-function setCountdown({ seconds, minutes, hours }) {
-  countdown.setAttribute("seconds", String(seconds));
-  countdown.setAttribute("minutes", String(minutes));
-  countdown.setAttribute("hours", String(hours));
-}
-
-let seconds = 600;
+let nSeconds = 600;
 const timeFromQuery = new URLSearchParams(location.search).get("t");
 if (timeFromQuery !== null) {
   try {
-    seconds = timeToSeconds(timeFromQuery);
+    nSeconds = timeToSeconds(timeFromQuery);
     setTimeout(() => countdown.start(), 0);
   } catch {
     console.warn(`query parameter t=${timeFromQuery} has an invalid format`);
   }
 }
-setCountdown(secondsToHMS(seconds));
+
+const { seconds, minutes, hours } = secondsToHMS(nSeconds);
+setTimeout(() => countdown.set(seconds, minutes, hours), 0);
